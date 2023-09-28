5d6de434ea34410370034b58d19e50786bf6e816
Alessandro Palermo <alessandro.palermo@mobilesoft.it>
2023-09-28 10:13:11 +0200

    Added accordion widget

---
+++ lib/widgets/accordion.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class Accordion extends StatefulWidget {
+  final String title;
+  final Widget child;
+  const Accordion({
+    super.key,
+    required this.title,
+    required this.child,
+  });
+
+  @override
+  State<Accordion> createState() => _AccordionState();
+}
+
+class _AccordionState extends State<Accordion> with TickerProviderStateMixin {
+  late bool _showChild = false;
+
+  /// The controller of the rotating animation of chevron when tapped
+  late final AnimationController _rotationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 200),
+    upperBound: 0.5,
+  );
+
+  /// The controller of expandable child widget when chevron is tapped
+  late final AnimationController _expandController = AnimationController(
+      vsync: this, duration: const Duration(milliseconds: 200));
+
+  @override
+  void dispose() {
+    _rotationController.dispose();
+    _expandController.dispose();
+    super.dispose();
+  }
+
+  void _changeTileView() {
+    if (_showChild) {
+      _rotationController.reverse(from: 0.5);
+      _expandController.reverse();
+    } else {
+      _rotationController.forward(from: 0.0);
+      _expandController.forward();
+    }
+    setState(() {
+      _showChild = !_showChild;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Material(
+            child: GestureDetector(
+              onTap: () {
+                _changeTileView();
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      widget.title,
+                      style: Theme.of(context).textTheme.labelMedium,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  ExcludeSemantics(
+                    child: IconButton(
+                      icon: RotationTransition(
+                        turns: Tween(begin: 0.0, end: 1.0)
+                            .animate(_rotationController),
+                        child: const Icon(
+                          Icons.keyboard_arrow_down,
+                          size: 16,
+                        ),
+                      ),
+                      onPressed: () {
+                        _changeTileView();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          FocusScope(
+            child: SizeTransition(
+              axisAlignment: 1.0,
+              sizeFactor: CurvedAnimation(
+                parent: _expandController,
+                curve: Curves.fastOutSlowIn,
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  widget.child,
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
